7a6ec9558fd66027faefa1714e00c2c946980d74
Alex Carl <alexjcarl97@gmail.com>
2024-08-12 21:09:27 -0400

    landing page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,24 @@
-
+import {useDispatch} from 'react-redux'
+import {createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom'
 
 import './App.css'
 import Landing from './components/LandingPage/landing'
+import { useEffect, useState } from 'react';
+
+
+
+
+
+
+
+
 
 function App() {
 
 
   return (
-    <>
-      <Landing />
-    </>
+
+    <Landing />
   )
 }
 
--- portfolio/src/components/LandingPage/landing.jsx
+++ portfolio/src/components/LandingPage/landing.jsx
@@ -4,10 +4,17 @@ import './landing.css'
 
 const Landing = () => {
     return (
+        <div id="landing">
         <div className="wrapper">
             <ParticlesComponent id='particles'/>
-            <h1 className="text">Hello World!</h1>
+            <div id="landing-txt">
+                <h3>Hey there!</h3>
+                <h1>I'm <span id="name-landing">Alex Carl</span>,</h1>
+                <h3>A full-stack developer who turns coffee into code and ideas into awesome web experiences.</h3>
+            </div>
+            <button>My Work</button>
         </div>
+            </div>
     )
 }
 
